c9965b02f5da2bc9ed5c242c0043b88f2b2dc2c9
duckaxe <>
2025-11-11 12:53:48 +0100

    Remove unused footer component

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -11,19 +11,16 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { RippleModule } from 'primeng/ripple';
 import { SidebarModule } from 'primeng/sidebar';
 
-import { AppFooterComponent } from './app.footer.component';
 import { AppLayoutComponent } from './app.layout.component';
 import { AppMenuComponent } from './app.menu.component';
 import { AppMenuitemComponent } from './app.menuitem.component';
 import { AppSidebarComponent } from './app.sidebar.component';
 import { AppTopBarComponent } from './app.topbar.component';
 
-//import { AppConfigModule } from './config/config.module';
 @NgModule({
     declarations: [
         AppMenuitemComponent,
         AppTopBarComponent,
-        AppFooterComponent,
         AppMenuComponent,
         AppSidebarComponent,
         AppLayoutComponent,
